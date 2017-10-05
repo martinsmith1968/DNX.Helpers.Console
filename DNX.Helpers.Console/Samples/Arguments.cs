bade7364e7e3b5496493ec5a6d71b842216cc96c
Martin Smith <martinsmith1968@gmail.com>
2017-10-05 10:51:36 +0100

    Added Sample usage classes

---
+++ DNX.Helpers.Console/Samples/Arguments.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+
+namespace SampleApp
+{
+    /// <summary>
+    /// Arguments class for command line
+    /// </summary>
+    public class Arguments
+    {
+        [Value(0, Required = true)]
+        public string FileName { get; set; }
+
+
+        [Option('v', "Verbose", DefaultValue = false, HelpText = "Generate more verbose output")]
+        public bool CheckFolderExists { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Arguments"/> class.
+        /// </summary>
+        public Arguments()
+        {
+        }
+    }
+}
---
+++ DNX.Helpers.Console/Samples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using DNX.CommandLine.Helpers.Exceptions;
+using DNX.Helpers.Assemblies;
+using DNX.Helpers.Console;
+using DNX.Helpers.Console.CommandLine;
+
+namespace SampleApp
+{
+    /// <summary>
+    /// Program controller class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>System.Int32.</returns>
+        private static int Main(string[] args)
+        {
+            try
+            {
+                var arguments = ParserHelper.Parse<Arguments>(args);
+
+                Run(arguments.Value);
+
+                return 0;
+            }
+            catch (ParserResultException<Arguments> ex)
+            {
+                var helpText = ParserHelper.BuildHelp(ex.Result);
+                Console.Error.WriteLine(helpText);
+
+                return 1;
+            }
+            catch (ReturnCodeException ex)
+            {
+                Console.WriteLine(ex);
+
+                return ex.ReturnCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return int.MaxValue;
+            }
+        }
+
+        /// <summary>
+        /// Runs the program using the specified arguments.
+        /// </summary>
+        /// <param name="arguments">The arguments.</param>
+        private static void Run(Arguments arguments)
+        {
+            var assemblyDetails = new AssemblyDetails();
+
+            ConsoleHelper.DisplayHeader(assemblyDetails);
+            ConsoleHelper.Display();
+
+            // TODO: Functionality here
+        }
+    }
+}
